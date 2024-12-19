2c3c737d042d351dc5c36db5126b3d492d9c6e5d
Teemu Niemi <teemuii.niemi@gmail.com>
2024-12-19 20:38:17 +0200

    m

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <div className='hero-container'>
       <div className='overlay'></div>
-      <h1>Most advanced technologies</h1>
+      <h1>Most advanced technologies <br /> <strong>This website is created for learning purposes</strong></h1>
     </div>
   )
 }
