bb97f36a6d88ce940cd31ffd372eaf416ed034e5
Teemu Niemi <teemuii.niemi@gmail.com>
2024-12-20 16:18:53 +0200

    undo form changes

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,57 +1,16 @@
 import React from 'react'
 import '../index.css'
 import './ContactForm.css'
-import { useState } from 'react'
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const formBody = new FormData();
-    formBody.append("form-name", "contact");
-    Object.keys(formData).forEach((key) => {
-      formBody.append(key, formData[key]);
-    });
-
-    try {
-      await fetch("/", {
-        method: "POST",
-        body: formBody,
-      });
-      alert("Message sent!");
-    } catch (err) {
-      alert("Failed to send message.");
-    }
-
-    setFormData({
-      name: "",
-      email: "",
-      phone: "",
-      message: "",
-    })
-  };
-
   return (
-    <form className='contact-form' name='contact' method='POST' data-netlify='true' onSubmit={handleSubmit} >
+    <form className='contact-form' name='contact' method='POST' data-netlify='true' >
       <h2>Get in touch</h2>
       <input 
         className='name-input'
         type="text"
         name='name'
         placeholder='Your Name'
-        value={formData.name}
-        onChange={handleChange}
         required
       />
       <div className='email-and-phone'>
@@ -60,8 +19,6 @@ const ContactForm = () => {
           type="email"
           name='email'
           placeholder='Email'
-          value={formData.email}
-          onChange={handleChange}
           required
         />
         <input
@@ -69,8 +26,6 @@ const ContactForm = () => {
           type="tel"
           name='phone'
           placeholder='Phone Number'
-          value={formData.phone}
-          onChange={handleChange}
           required 
         />
       </div>
@@ -78,8 +33,6 @@ const ContactForm = () => {
         className='textarea-input'
         name='message'
         placeholder='Your Message'
-        value={formData.message}
-        onChange={handleChange}
         required
       ></textarea>
       <button className='submit-button' type='submit'>Send</button>
